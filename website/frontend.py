4e526d269af2efb9ef066e315fe4bfaaa8370d5b
Kae Bartlett <kae@voxelfox.co.uk>
2022-07-04 22:52:21 +0100

    Switch out decorator

--- website/frontend.py
+++ website/frontend.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from aiohttp.web import HTTPFound, Request, Response, RouteTableDef
 import aiohttp_session
 from aiohttp_jinja2 import template
@@ -8,23 +10,38 @@ from discord.ext import vbu
 routes = RouteTableDef()
 
 
+def add_standard_args():
+    def inner(func):
+        async def wrapper(request: Request):
+            d: Union[None, Response, dict] = await func(request)
+            if d is None:
+                d = {}
+            elif isinstance(d, Response):
+                return d
+            d['session'] = await aiohttp_session.get_session(request)
+            d['request'] = request
+            return d
+        return wrapper
+    return inner
+
+
 @routes.get("/")
 @routes.get("/index")
 @template("index.htm.j2")
-@vbu.web.add_discord_arguments()
+@add_standard_args()
 async def index(request: Request):
     return {}
 
 
 @routes.get("/leaderboard")
 @template("leaderboard.htm.j2")
-@vbu.web.add_discord_arguments()
+@add_standard_args()
 async def leaderboard(request: Request):
     return {}
 
 
 @routes.get("/videos")
 @template("videos.htm.j2")
-@vbu.web.add_discord_arguments()
+@add_standard_args()
 async def videos(request: Request):
     return {}
